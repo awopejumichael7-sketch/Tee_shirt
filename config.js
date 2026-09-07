a7dfd71d3a4477069517c92f453096272ed99715
awopejumichael7-sketch <awopejumichael7@gmail.com>
2026-09-07 18:47:44 +0100

    Update SIZES array to fewer options
    Reduced the available sizes from nine to five.

--- config.js
+++ config.js
@@ -34,7 +34,7 @@ const CONFIG = {
   COLLECTION_METHODS: ["Church Collection"],
 
   // Sizes offered for every design. Change centrally.
-  SIZES: ["S", "M", "L", "XL", "XXL", "XXXL", "XXXXL", "XXXXXL", "XXXXXXL"],
+  SIZES: ["S", "M", "L", "XL", "XXL"],
 
   // ---------------------------------------------------------------------
   // PRODUCT CATALOGUE (day-one seed data)
